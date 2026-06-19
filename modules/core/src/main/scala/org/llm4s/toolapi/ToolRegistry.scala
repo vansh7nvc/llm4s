16d7bd4787f498a58c7b3cb4e7622871b65329ee
Vansh7nvc <vanshbest1@gmail.com>
2026-06-19 15:01:54 +0530

    fix: clear interrupt flag before returning thread to pool
    Add try/finally with Thread.interrupted() in the timeout Future to
    read-and-clear the interrupt flag. Without this, an interrupted thread
    could be returned to ExecutionContext.global's ForkJoinPool with the
    flag still set, causing unrelated tasks scheduled on that thread to
    fail spuriously.
    
    Fixes review feedback on #1069.

--- modules/core/src/main/scala/org/llm4s/toolapi/ToolRegistry.scala
+++ modules/core/src/main/scala/org/llm4s/toolapi/ToolRegistry.scala
@@ -178,7 +178,8 @@ class ToolRegistry(initialTools: Seq[ToolFunction[_, _]]) {
 
         val runFuture = Future {
           workerThread = Thread.currentThread()
-          blocking(runOneAttempt(request))
+          try blocking(runOneAttempt(request))
+          finally Thread.interrupted() // read-and-clear: don't return a dirty thread to the pool
         }
 
         val scheduled = ToolRegistry.timeoutScheduler.schedule(
